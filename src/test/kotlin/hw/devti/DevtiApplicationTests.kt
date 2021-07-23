b2861140d8e975bd145ccddc986b147ec9812def
Hyeongwon-up <leehwdatabase@gmail.com>
2021-07-24 01:29:05 +0900

    first

---
+++ src/test/kotlin/hw/devti/DevtiApplicationTests.kt
@@ -0,0 +1,13 @@
+package hw.devti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevtiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
