0dc52ff5a0075f71fb10cda7808dbeaf86155655
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-01 22:05:05 +0900

    kotest 연습

---
+++ src/test/kotlin/hw/devti/domain/advertisement/api/AdvertisementTest.kt
@@ -0,0 +1,21 @@
+package hw.devti.domain.advertisement.api
+
+import hw.devti.domain.advertisement.dao.AdvertisementRepository
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+
+internal class AdvertisementTest: StringSpec(
+){
+
+
+
+}
