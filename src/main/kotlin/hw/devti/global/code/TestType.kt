7ea46b03fa42e816fa25839d6d6b1ee6ffa85783
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-02 03:47:32 +0900

    Enum 추가

---
+++ src/main/kotlin/hw/devti/global/code/TestType.kt
@@ -0,0 +1,25 @@
+package hw.devti.global.code
+
+val testTypes: Array<TestType> = TestType.values()
+
+enum class TestType(
+    private val value: Int
+):DevtiEnumerable {
+
+    TYPE_COMMON_1(0), //Common type 1
+    TYPE_COMMON_2(1), //Common type 2
+    TYPE_COMMON_3(2), //Common type 3
+    TYPE_COMMON_4(3), //Common type 4
+    TYPE_MOM_CAFE_1(4), //Mom cafe type
+    ;
+
+
+    override fun getValue(): Int {
+        return value
+    }
+
+    override fun getKey(): String {
+        return name
+    }
+
+}
